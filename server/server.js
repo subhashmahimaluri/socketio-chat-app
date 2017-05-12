aa97cc7c49d218e4bb9b89cc9fa661e5cdcafa47
Subhash MAhimaluri <subhash.mahimaluri@mindtree.com>
2017-05-12 19:52:11 +0530

    create events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from : "Sam",
+    text : "message from Sam",
+    createdAt: 12333
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('user was disconnected');
   });
