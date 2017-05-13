92a97369c0d4dd9a5ff831396a7464648b041f58
Subhash MAhimaluri <subhash.mahimaluri@mindtree.com>
2017-05-13 19:15:20 +0530

    Add Css for Chat app

--- public/js/index.js
+++ public/js/index.js
@@ -15,13 +15,15 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
+var messageTextbox = jQuery('[name=message]');
+
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('');
   });
 });
 
@@ -40,12 +42,16 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('sending location ...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.')
   });
 
