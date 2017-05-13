ef273aedd0a9c7cbe480d95be855d4ec3f262b66
Subhash MAhimaluri <subhash.mahimaluri@mindtree.com>
2017-05-13 20:35:07 +0530

    Add mustache.js for messages templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,9 +10,14 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  jQuery('#messages').append(li);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
 });
 
 var messageTextbox = jQuery('[name=message]');
@@ -29,12 +34,14 @@ jQuery('#message-form').on('submit', function (e) {
 
 socket.on('newLocationMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My Current Location</a>')
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
 })
 
 var locationButton = jQuery('#send-location');
