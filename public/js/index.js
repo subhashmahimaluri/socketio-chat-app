c1aa4bb05292697b17049738fc1214fbda265edb
Subhash MAhimaluri <subhash.mahimaluri@mindtree.com>
2017-05-13 18:29:06 +0530

    Add geo location support via geo location API

--- public/js/index.js
+++ public/js/index.js
@@ -23,4 +23,30 @@ jQuery('#message-form').on('submit', function (e) {
   }, function () {
 
   });
+});
+
+socket.on('newLocationMessage', function(message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My Current Location</a>')
+  li.text(`${messages.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
 })
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Unable to fetch location.')
+  });
+
+});
