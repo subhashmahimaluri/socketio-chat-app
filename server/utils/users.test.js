155fe4d3e7637e47618a80a52813a825459eb2bd
Subhash MAhimaluri <subhash.mahimaluri@mindtree.com>
2017-05-14 12:47:43 +0530

    Add Users class and update userList event

--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -3,6 +3,26 @@ const expect = require('expect');
 const {Users} = require('./users');
 
 describe('Users', () => {
+  var users;
+
+  beforeEach(() =>{
+    users = new Users();
+    users.users = [{
+      id: '1',
+      name: 'Subhash',
+      room: 'Node'
+    },
+    {
+      id: '2',
+      name: 'Sumedha',
+      room: 'Node'
+    },{
+      id: '3',
+      name: 'Aadhya',
+      room: 'React'
+    }];
+  });
+
   it('should add new users', () => {
     var users = new Users();
     var user = {
@@ -13,5 +33,46 @@ describe('Users', () => {
     var resUser = users.addUser(user.id, user.name, user.room);
 
     expect(users.users).toEqual([user]);
+  });
+
+  it('should remove a user', () => {
+    var userId = '1';
+    var user = users.removeUser(userId);
+
+    expect(user.id).toBe(userId);
+    expect(users.users.length).toBe(2);
+  });
+
+  it('should not remove a user', () => {
+    var userId = '99';
+    var user = users.removeUser(userId);
+
+    expect(user).toNotExist();
+    expect(users.users.length).toBe(3);
+  });
+
+  it('should find user', () => {
+    var userId = '2';
+    var user = users.getUser(userId);
+
+    expect(user.id).toBe(userId);
+  });
+
+  it('should not find user', () => {
+    var userId = '99';
+    var user = users.getUser(userId);
+
+    expect(user).toNotExist();
+  });
+
+  it('should return names for node course', () => {
+    var userList = users.getUserList('Node');
+
+    expect(userList).toEqual(['Subhash', 'Sumedha']);
+  });
+  it('should return names for react course', () => {
+    var userList = users.getUserList('React');
+
+    expect(userList).toEqual(['Aadhya']);
   });
 });
